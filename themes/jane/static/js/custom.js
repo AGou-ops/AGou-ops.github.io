664d1e7608de10b70b9087b1708567f4bec10df9
AGou-ops <suofeiyaxx@gmail.com>
2020-12-06 12:05:02 +0800

    rebuilding site 2020年 12月 06日 星期日 12:05:02 CST

--- themes/jane/static/js/custom.js
+++ themes/jane/static/js/custom.js
@@ -1,29 +1,29 @@
 // 1. Instantiate the search
 const search = instantsearch({
-  indexName: 'blog',
-  searchClient: algoliasearch('USNPT8OERQ', '27e7e794fae67cc7380e39e0209455f8'),
+    indexName: 'blog',
+    searchClient: algoliasearch('USNPT8OERQ', '27e7e794fae67cc7380e39e0209455f8'),
 });
 
 search.addWidgets([
-  // 2. Create an interactive search box
-  instantsearch.widgets.searchBox({
-    container: '#searchbox',
-    placeholder: 'Search for products',
-  }),
+    // 2. Create an interactive search box
+    instantsearch.widgets.searchBox({
+        container: '#searchbox',
+        placeholder: 'Search for products',
+    }),
 
-  // 3. Plug the search results into the product container
-  instantsearch.widgets.hits({
-    container: '#products',
-    templates: {
-      item: '{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}',
-    },
-  }),
+    // 3. Plug the search results into the product container
+    instantsearch.widgets.hits({
+        container: '#products',
+        templates: {
+            item: '{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}',
+        },
+    }),
 
-  // 4. Make the brands refinable
-  instantsearch.widgets.refinementList({
-    container: '#brand',
-    attribute: 'brand',
-  }),
+    // 4. Make the brands refinable
+    instantsearch.widgets.refinementList({
+        container: '#brand',
+        attribute: 'brand',
+    }),
 ]);
 
 // 5. Start the search!
