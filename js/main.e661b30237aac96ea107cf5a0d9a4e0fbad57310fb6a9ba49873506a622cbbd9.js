b54237e459a9a85924c3ee25cf609f457608b0cb
AGou-ops <AGou-ops@users.noreply.github.com>
2021-12-24 15:13:38 +0000

    deploy: b7067a84c9f6f3162b2fe88fdf8843d43235f3ba

--- js/main.e661b30237aac96ea107cf5a0d9a4e0fbad57310fb6a9ba49873506a622cbbd9.js
+++ js/main.e661b30237aac96ea107cf5a0d9a4e0fbad57310fb6a9ba49873506a622cbbd9.js
@@ -217,6 +217,16 @@ function mapLang(name) {
 }
 
 
+var responsiveTable = function() {
+    const tables = document.querySelectorAll('.post-content table:not(.lntable)');
+    for (let i = 0; i < tables.length; i++) {
+      const table = tables[i];
+      const wrapper = document.createElement('div');
+      wrapper.className = 'table-wrapper';
+      table.parentElement.replaceChild(wrapper, table);
+      wrapper.appendChild(table);
+    }
+  }
 /* main */
 $(document).ready(function() {
     backToTop();
@@ -224,6 +234,7 @@ $(document).ready(function() {
     toc();
     headerAnchor();
     fnTooltip();
+    responsiveTable();
+    highlight();
 });
 
-highlight();
